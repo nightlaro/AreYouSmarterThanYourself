aa4cdda642e3722e60dfdb7c3d6b4066c329037e
nightlaro <mickocabacungan@gmail.com>
2021-08-24 13:18:50 -0700

    Type safety for Resource ID marked as Drawable Resource

--- app/src/main/java/com/example/areyousmarterthanyourself/CardData.kt
+++ app/src/main/java/com/example/areyousmarterthanyourself/CardData.kt
@@ -1,9 +1,10 @@
 package com.example.areyousmarterthanyourself
 
 import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
 
 data class CardData(
-    val resId: Int,
+    val resId: DrawableRes,
     var backgroundImage: Drawable?,
     val id: ValidCards,
     var matched: Boolean)
