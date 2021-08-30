7ddd9f4c0eb8cedb30c40da9909cb54e9f48791c
nightlaro <mickocabacungan@gmail.com>
2021-08-30 16:48:05 -0700

    Refactor GameScoreManager so we only have one instance

--- app/src/main/java/com/example/areyousmarterthanyourself/GameScoreManager.kt
+++ app/src/main/java/com/example/areyousmarterthanyourself/GameScoreManager.kt
@@ -1,18 +1,32 @@
 package com.example.areyousmarterthanyourself
 
+import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.activity.viewModels
 import androidx.core.content.edit
+import androidx.lifecycle.MutableLiveData
 import androidx.preference.PreferenceManager
 
 class GameScoreManager(val context : Context) {
+
+    companion object {
+        lateinit var instance : GameScoreManager
+        private var _instance : GameScoreManager? = null
+        fun initialize(app: Application) {
+            if (_instance == null) {
+                _instance = GameScoreManager(app)
+                instance = _instance!! //we know for a fact we set this to GSM ^ above line
+            }
+        }
+    }
 
     private val sharedPref : SharedPreferences by lazy {
         PreferenceManager.getDefaultSharedPreferences(context)
     }
 
-    private var historyScore = listOf<String>()
+    val historyScore : MutableLiveData<List<String>> by lazy {
+        MutableLiveData<List<String>>(listOf())
+    }
 
     fun saveScore(score: Int) {
         sharedPref.edit {
@@ -26,17 +40,18 @@ class GameScoreManager(val context : Context) {
 
     fun saveScoreHistory() {
         getScoreHistory().let {
-            historyScore = it
+            historyScore.value = it
         }
-        val historyScoreSet = historyScore.toMutableSet()
+        val historyScoreSet = historyScore.value!!.toMutableSet()
         historyScoreSet.add(getScore().toString())
         sharedPref.edit {
             putStringSet("SCORE_HISTORY", historyScoreSet)
         }
+        historyScore.value = historyScoreSet.toList()
     }
 
     fun getScoreHistory(): List<String> {
-        val savedHistory = sharedPref.getStringSet("SCORE_HISTORY", historyScore.toMutableSet())
+        val savedHistory = sharedPref.getStringSet("SCORE_HISTORY", historyScore.value!!.toMutableSet())
         return savedHistory?.toList() ?: emptyList()
     }
 
